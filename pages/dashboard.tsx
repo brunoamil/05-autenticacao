1b148250f80e446044d4407f9393e132c123e8be
Bruno Lima <francisco.bruno@armateus.com.br>
2022-02-04 10:16:39 -0300

    tratativa de erros na autenticacao

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { api } from "../services/api";
+import { setupApiClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard(){
     const { user } = useContext(AuthContext);
@@ -17,3 +21,12 @@ export default function Dashboard(){
         </>
     )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupApiClient(ctx);
+    const response = await apiClient.get('/me');
+    console.log(response.data);
+    return {
+        props: {}
+    }
+})
