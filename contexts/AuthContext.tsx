3888d6333a37ed16d6e770763d4782c4bc0ba388
Bruno Lima <francisco.bruno@armateus.com.br>
2022-02-04 18:38:30 -0300

    trabalhando com broadcast e redirect

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -16,7 +16,8 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>;
+    signIn: (credentials: SignInCredentials) => Promise<void>;
+    signOut: () => void;
     isAuthenticated: boolean;
     user: User;
 }
@@ -27,15 +28,36 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel;
+
 
 export function signOut(){
+    destroyCookie(undefined, 'nextauth.token');
     destroyCookie(undefined, 'nextauth.refreshToken');
-    destroyCookie(undefined, 'nextauth.refreshToken');
+
+    authChannel.postMessage('signOut');
+
     Router.push('/');
 }
 export function AuthProvider({  children }: AuthProviderProps){
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        authChannel = new BroadcastChannel('auth');
+
+        authChannel.onmessage = (message) => {
+            switch (message.data) {
+                case 'signOut':
+                    signOut();
+                    authChannel.close();
+                    break;
+                    default:
+                    break;
+                
+            }
+        }
+    }, []);
 
     useEffect(() => {
         const {'nextauth.token': token } = parseCookies();
@@ -55,7 +77,6 @@ export function AuthProvider({  children }: AuthProviderProps){
         }
     }, []);
 
-    console.log('USERS SET USER', user)
 
     async function signIn({ email, password }: SignInCredentials) {
         try {
@@ -87,13 +108,15 @@ export function AuthProvider({  children }: AuthProviderProps){
 
             api.defaults.headers['Authorization'] = `Bearer ${token}`;
             Router.push('/dashboard');
+            //authChannel.postMessage('signIn');
+
         } catch(err){
             alert(err);
         }
     }
 
     return (
-        <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
+        <AuthContext.Provider value={{signIn, signOut, isAuthenticated, user}}>
             {children}
         </AuthContext.Provider>
     )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,13 +1,15 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard(){
-    const { user } = useContext(AuthContext);
+    const { user, signOut } = useContext(AuthContext);
 
     useEffect(() => {
         api.get('/me').then(response => console.log(response))
@@ -18,6 +20,10 @@ export default function Dashboard(){
         <>
             <h1>Dashboard</h1>
             {user?.email}
+            <Can permissions={['metrics.list']}>
+                <div>Métricas</div>
+                <button onClick={signOut}>Sign Out</button>
+            </Can>
         </>
     )
 }
